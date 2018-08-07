9f867ff960a6706c63559194954e0c67fc6deb17
lucas <lucas@DESKTOP-AE014FA.localdomain>
2018-08-07 00:45:29 -0300

    catálogo fútbol listo

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,103 @@
-console.log("hola culiao")
-function openNav(){
-	document.getElementById("header").classList.remove("hidden-mobile")
-	document.getElementById("open-nav").classList.add("hidden")
-	document.getElementById("close-nav").classList.remove("hidden")
-}
-function closeNav(){
-	document.getElementById("header").classList.add("hidden-mobile")
-	document.getElementById("open-nav").classList.remove("hidden")
-	document.getElementById("close-nav").classList.add("hidden")
-}
+//start navigation function
+$('#open-nav').click(function() {
+	$('header').removeClass('hidden-mobile');
+	$('#open-nav').addClass('hidden');
+	$('#close-nav').removeClass('hidden');
+});
+
+$('#close-nav').click(function() {
+	$('header').addClass('hidden-mobile');
+	$('#open-nav').removeClass('hidden');
+	$('#close-nav').addClass('hidden');
+});
+//end navigation function
+
+//start modal function
+$('#open-modal-1').click(function() {
+	$('#modal-1').removeClass("hidden");
+});
+$('#close-modal-1').click(function() {
+	$('#modal-1').addClass('hidden');
+});
+
+
+$('#open-modal-2').click(function() {
+	$('#modal-2').removeClass("hidden");
+});
+$('#close-modal-2').click(function() {
+	$('#modal-2').addClass('hidden');
+});
+
+
+$('#open-modal-3').click(function() {
+	$('#modal-3').removeClass("hidden");
+});
+$('#close-modal-3').click(function() {
+	$('#modal-3').addClass('hidden');
+});
+
+
+$('#open-modal-4').click(function() {
+	$('#modal-4').removeClass("hidden");
+});
+$('#close-modal-4').click(function() {
+	$('#modal-4').addClass('hidden');
+});
+
+
+$('#open-modal-5').click(function() {
+	$('#modal-5').removeClass("hidden");
+});
+$('#close-modal-5').click(function() {
+	$('#modal-5').addClass('hidden');
+});
+
+
+$('#open-modal-6').click(function() {
+	$('#modal-6').removeClass("hidden");
+});
+$('#close-modal-6').click(function() {
+	$('#modal-6').addClass('hidden');
+});
+
+
+$('#open-modal-7').click(function() {
+	$('#modal-7').removeClass("hidden");
+});
+$('#close-modal-7').click(function() {
+	$('#modal-7').addClass('hidden');
+});
+
+
+$('#open-modal-8').click(function() {
+	$('#modal-8').removeClass("hidden");
+});
+$('#close-modal-8').click(function() {
+	$('#modal-8').addClass('hidden');
+});
+
+
+$('#open-modal-9').click(function() {
+	$('#modal-9').removeClass("hidden");
+});
+$('#close-modal-9').click(function() {
+	$('#modal-9').addClass('hidden');
+});
+
+
+$('#open-modal-10').click(function() {
+	$('#modal-10').removeClass("hidden");
+});
+$('#close-modal-10').click(function() {
+	$('#modal-10').addClass('hidden');
+});
+
+
+$('#open-modal-11').click(function() {
+	$('#modal-11').removeClass("hidden");
+});
+$('#close-modal-11').click(function() {
+	$('#modal-11').addClass('hidden');
+});
+
+//end modal function
